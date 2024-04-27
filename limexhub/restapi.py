8927a1aaa17b188d5b599f628aaf78f7c37ed2b3
Vyacheslav Arbuzov <arbuzov1989@gmail.com>
2024-04-27 19:06:33 +0500

    v.0.1.21

--- limexhub/restapi.py
+++ limexhub/restapi.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 from typing import Optional,Dict
+from datetime import datetime
+
  
 class RestAPI:
     def __init__(self, base_url="https://hub.limex.com/v1", token=None):
@@ -18,50 +20,74 @@ class RestAPI:
         params = {'assets': assets}
         return pd.DataFrame(self._get(endpoint, params=params))
    
-    def candles(self, symbol, from_date, timeframe):
-        endpoint = "candles"
-        params = {'symbol': symbol, 'from': from_date, 'timeframe': timeframe}
-        res = pd.DataFrame(self._get(endpoint, params=params))
-        if timeframe>=3:
-            res['ts'] = pd.to_datetime(res['ts'], utc=True).dt.date
-        else:
-            res['ts'] = pd.to_datetime(res['ts'], utc=True)
-        return res
+    def candles(self, symbol, from_date='2020-01-01',to_date=datetime.today().strftime('%Y-%m-%d'), timeframe=3):
+        all_data = pd.DataFrame()
+        if isinstance(symbol, str):
+            symbol = [symbol]
+        for ticker in symbol:
+            endpoint = "candles"
+            params = {'symbol': ticker, 'from': from_date, 'to': to_date, 'timeframe': timeframe}
+            res = pd.DataFrame(self._get(endpoint, params=params))
+            res['ts'] = pd.to_datetime(res['ts'], utc=False)
+            res = res.sort_values(by=['ts'])
+            res = res.set_index('ts')
+            if timeframe>=3:
+                res.index = res.index.normalize()    
+            if len(symbol)>1:         
+                res[f'{ticker}'] = res['c']
+                if all_data.empty:
+                    all_data = res[[f'{ticker}']]
+                else:
+                    all_data = all_data.join(res[[f'{ticker}']], how='outer')
+            else:
+                all_data = res
+
+        all_data.index.name = 'Date'
+        return all_data
    
-    def fundamental(self, symbol, to_date, from_date, fields):
+   
+    def fundamental(self, symbol='', from_date='2020-01-01',to_date=datetime.today().strftime('%Y-%m-%d'), fields='ebitda', quarter='Q2-2023'):
         endpoint = "fundamental"
-        params = {'symbol': symbol, 'to': to_date, 'from': from_date, 'fields': fields}
+        if symbol=='':
+            params = {'from': from_date,'to': to_date, 'fields': fields,'quarter': quarter}
+        else:
+            params = {'symbol': symbol, 'from': from_date,'to': to_date, 'fields': fields}            
         res = pd.DataFrame(self._get(endpoint, params=params))
         res['date'] = pd.to_datetime(res['date'], utc=True).dt.date
         return res
    
-    def news(self, symbol, to_date, from_date):
+    def news(self, symbol, from_date='2020-01-01',to_date=datetime.today().strftime('%Y-%m-%d')):
         endpoint = "news"
-        params = {'symbol': symbol, 'to': to_date, 'from': from_date}
+        params = {'symbol': symbol, 'from': from_date,'to': to_date}
         res = pd.DataFrame(self._get(endpoint, params=params))
         res['created'] = pd.to_datetime(res['created'], utc=True)
         return res
    
-    def events(self, symbol, to_date, from_date, event_type):
+    def events(self, symbol, from_date='2020-01-01',to_date=datetime.today().strftime('%Y-%m-%d'), event_type='dividends'):
         endpoint = "events"
-        params = {'symbol': symbol, 'to': to_date, 'from': from_date, 'type': event_type}
+        params = {'symbol': symbol, 'from': from_date, 'to': to_date,'type': event_type}
         res = pd.DataFrame(self._get(endpoint, params=params))
         res['buy_date'] = pd.to_datetime(res['buy_date'], utc=True).dt.date
         return res
    
-    def signals(self, vendor, model, symbol, from_date):
+    def signals(self, vendor='boosted', model='50678d2d-fd0f-4841-aaee-7feac83cb3a1', symbol='', from_date='2020-01-01',to_date=datetime.today().strftime('%Y-%m-%d')):
         endpoint = "signals"
-        params = {'vendor': vendor, 'model': model, 'symbol': symbol, 'from': from_date}
+        if symbol=='':
+            params = {'vendor': vendor, 'model': model, 'from': from_date,'to': to_date}
+        else:
+            params = {'vendor': vendor, 'model': model, 'symbol': symbol, 'from': from_date,'to': to_date}
         res = pd.DataFrame(self._get(endpoint, params=params))
         res['trade_date'] = pd.to_datetime(res['trade_date'], utc=True).dt.date
+        res = res.set_index('trade_date')
+        res.index.name = 'Date'
         return res
  
-    def models(self, vendor):
+    def models(self, vendor='boosted'):
         endpoint = "models"
         params = {'vendor': vendor}
-        return self._get(endpoint, params=params)
+        return pd.DataFrame(self._get(endpoint, params=params))    
 
-    def altercandles(self, symbol, to_date, from_date, timeframe):
+    def altercandles(self, symbol, from_date='2020-01-01',to_date=datetime.today().strftime('%Y-%m-%d'), timeframe=3):
         endpoint = "altercandles"
         params = {'symbol': symbol, 'to': to_date, 'from': from_date, 'timeframe': timeframe}
         res = pd.DataFrame(self._get(endpoint, params=params))
@@ -69,5 +95,12 @@ class RestAPI:
             res['ts'] = pd.to_datetime(res['ts'], utc=True).dt.date
         else:
             res['ts'] = pd.to_datetime(res['ts'], utc=True)
-        return res
+        return res.sort_values(by=['ts'])
  
+#if __name__ == "__main__":
+#    api_token = ""
+#    client = RestAPI(token=api_token)#
+#    client.fundamental()
+#    client.signals(symbol="")
+#    candles = client.candles(['AMZN','AAPL'],from_date="2022-01-01",to_date="2024-01-01")
+#    print(candles)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='limexhub',
-    version='0.1.6',
+    version='0.1.22',
     packages=find_packages(),
     include_package_data=True,
     description='A simple API wrapper for Limex DataHub',
